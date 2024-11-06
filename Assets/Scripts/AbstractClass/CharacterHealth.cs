01d74171f40190f9b87d3d3a2ce88d3dd03520b0
Lưu Thành Đạt <107013537+luuthanhdat25@users.noreply.github.com>
2024-11-06 22:42:08 +0700

    Enemy (#14)
    * Add Night Borne Enemy
    
    * Stop code fly demon
    
    * Commit before merge with Manhld
    
    * Commit 23/10
    
    * Reverse and resolve conflit
    
    * Add EnemySO script

--- Assets/Scripts/AbstractClass/CharacterHealth.cs
+++ Assets/Scripts/AbstractClass/CharacterHealth.cs
@@ -22,7 +22,6 @@ namespace AbstractClass
         protected int damage;
 
         private void OnEnable() => Reborn();
-
         
         public virtual void Reborn()
         {
@@ -74,5 +73,4 @@ namespace AbstractClass
 
         public override void SetDamage(int value) => damage = value;
     }
-
 }
